cbc4c0808f36c787df6ff8a88668ecce8fd94f17
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-09-11 19:06:21 -0700

    RPM.FILES setting for usersguide

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -9,5 +9,5 @@ SUMMARY_ARCHITECTURE	= i386, x86_64
 
 ROLL_REQUIRES		= base kernel os
 ROLL_CONFLICTS		=
-PM.FILES       = /var/www/html/roll-documentation/$(ROLL)/*
+RPM.FILES       	= /var/www/html/roll-documentation/$(ROLL)/$(VERSION)
 
